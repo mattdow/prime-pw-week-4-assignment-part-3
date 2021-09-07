fccda95050b97f0d584bbb3301e91108f2505322
Matt Dow <matt.dow.vo@gmail.com>
2021-09-07 18:35:43 -0500

    Completed required goals with basic testing

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,33 @@
 console.log('***** Cart Functions *****');
-// Make sure to test all functions here in the JS file!
-// We want to see how you are testing your code!!!
+
+let basket = [];
+function addItem(item){
+  basket.push(item);
+  return true;
+} // end addItem function
+
+console.log(`Basket is now ${basket}`);
+console.log('Adding yogurt (expect true)', addItem('yogurt'));
+console.log(`Basket is now ${basket}`);
+console.log('Adding sriracha (expect true)', addItem('sriracha'));
+console.log(`Basket is now ${basket}`);
+
+function listItems(){
+  for (item in basket){
+    console.log(basket[item]);
+  } // end of loop of basket items
+} //  end of listItems
+
+listItems();
+console.log('Adding peaches (expect true)', addItem('peaches'));
+listItems();
+console.log(`Basket is now ${basket}`);
+
+function empty(){
+  basket = [];
+  return false;
+}
+
+console.log('Emptying the basket (expect false)', empty());
+console.log(`Basket is now ${basket}`);
+console.log('Testing full list of items', listItems());
